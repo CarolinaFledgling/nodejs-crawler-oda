b92a015e35b8a3aff4f8b8ae28a03f1fbeabd57b
Karolina Kulinska <karolina.kulinska89@gmail.com>
2023-02-26 00:50:51 +0100

    fixed bug where crawl fn  check the same page instead moving onto the next link, added variable pageCount to keep track of the number of the pages that have been crawled so far

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,9 @@ import fs from "fs";
 import fetchHtml from "./util/fetchHtml.mjs";
 import parseHtml from "./util/parseHtml.mjs";
 import extractProductData from "./util/extractProductData.mjs";
+import getLinks from "./util/getLinks.mjs";
+
+let pageCount = 1; // keep track of the number of pages that have been crawled so far.
 
 async function crawl(
   url,
@@ -13,77 +16,49 @@ async function crawl(
   visitedPages,
   products
 ) {
-  if (visitedPages.length >= maxPages) {
-    console.log(`Reached max pages ${maxPages}`);
-    return;
-  }
-
   if (visitedPages.includes(url)) {
     console.log(`Already visited ${url}`);
     return;
+  } else {
+    visitedPages.push(url);
   }
 
   console.log(`Crawling ${url}`);
 
   try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(
-        `Error fetching products from url:${url}, response status:${response.status}`
-      );
-    }
-
     const html = await fetchHtml(url);
     const $ = parseHtml(html);
 
-    //Get array of all the links on the page
-    const links = $("a").get();
+    //Get array of links must not be undefined,contain  "/categories/" and must not have already been visited
+    const links = getLinks($, visitedPages);
+
+    // Get all the product elements on the page
+    const productsElements = $(productClassName);
+
+    for (const productEl of productsElements) {
+      const productData = extractProductData($, productEl, url);
+      products.push(productData);
+    }
 
     for (let i = 0; i < links.length; i++) {
-      const link = links[i];
-      let href = $(link).attr("href");
+      pageCount++;
 
-      if (!href) {
-        continue;
+      if (pageCount <= maxPages) {
+        // Wait for delay before making the next request
+        await new Promise((resolve) => setTimeout(resolve, delay));
+
+        //Recursively crawl the link
+        await crawl(
+          links[i],
+          maxPages,
+          delay,
+          productClassName,
+          visitedPages,
+          products
+        );
+      } else {
+        console.log("limit reached");
       }
-
-      if (href.startsWith("/")) {
-        href = "https://oda.com" + href;
-      }
-
-      //Skip links that do not contain "/categories/"
-      if (!href.includes("/categories/")) {
-        continue;
-      }
-
-      if (visitedPages.length >= maxPages) {
-        console.log(`Reached max pages (${maxPages})`);
-        return;
-      }
-
-      visitedPages.push(href);
-
-      // Get all the product elements on the page
-      const productsElements = $(productClassName);
-
-      for (const productEl of productsElements) {
-        const productData = extractProductData($, productEl, url);
-        products.push(productData);
-      }
-
-      // Wait for delay before making the next request
-      await new Promise((resolve) => setTimeout(resolve, delay));
-
-      //Recursively crawl the link
-      await crawl(
-        url,
-        maxPages,
-        delay,
-        productClassName,
-        visitedPages,
-        products
-      );
     }
 
     console.log(`Crawling stopped`);
---
+++ src/util/getLinks.mjs
@@ -0,0 +1,22 @@
+function getLinks($, visitedPages) {
+  return $("a")
+    .get()
+    .map((element) => {
+      let href = $(element).attr("href");
+
+      if (!href) {
+        return undefined;
+      }
+
+      if (href.startsWith("/")) {
+        href = "https://oda.com" + href;
+      }
+      return href;
+    })
+    .filter(
+      (href) =>
+        href && href.includes("/categories/") && !visitedPages.includes(href)
+    );
+}
+
+export default getLinks;
